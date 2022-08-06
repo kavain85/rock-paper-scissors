9e943e1f1328954e9d9e10b8d18d79cb7720bee6
kavain85 <gruye2@gmail.com>
2022-08-05 20:34:28 -0700

    Add playRound function

--- script.js
+++ script.js
@@ -10,4 +10,27 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    // return results, do not console.log!
+    // let playerSelection.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
+    const winMessage = 'You win! '+computerSelection+' beats '+(playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase())+'!';
+    const loseMessage = 'You lose! '+computerSelection+' beats '+(playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase())+'!';
+    const drawMessage = 'Round is a draw!'
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
+        return loseMessage;
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
+        return loseMessage;
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock') {
+        return loseMessage;
+    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return drawMessage;
+    } else {
+        return winMessage;
+    }
+}
+
+//  Use these to test function!
+
+const playerSelection = 'rOcK';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
