b95d50650ab005cf990180d8df32f14b90d42fb8
kavain85 <gruye2@gmail.com>
2022-08-05 19:45:40 -0700

    Add computer choice function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+This is the Rock, Paper, Scissors exercise for The Odin Project. It is meant to test my ability to create a simple game from scratch using what I have learned from previous modules, and my ability to problem solve and find the information that I need to make it work.
---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()*3);
+    
+    if (computerChoice === 0) {
+        return 'Rock';
+    } else if (computerChoice === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+console.log(getComputerChoice());
