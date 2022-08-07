ed910ed15eefeb9196c5ede8d8ab3f615775e53a
kavain85 <gruye2@gmail.com>
2022-08-06 18:29:26 -0700

    Add scoring system, refactoring

--- script.js
+++ script.js
@@ -11,30 +11,50 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase()
-    const winMessage = 'You win! '+playerChoice+' beats '+computerSelection+'!';
-    const loseMessage = 'You lose! '+computerSelection+' beats '+playerChoice+'!';
-    const drawMessage = 'Round is a draw! You chose: '+playerChoice+', Computer chose: '+computerSelection+'.';
 
-    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') {
+    let playerChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase();
+    
+    const winMessage = `You win! ${playerChoice} beats ${computerSelection}!`;
+    const loseMessage = `You lose! ${computerSelection} beats ${playerChoice}!`;
+    const drawMessage = `Round is a draw! You chose: ${playerChoice}, Computer chose: ${computerSelection}`;
+
+    if (playerChoice === 'Rock' && computerSelection === 'Paper'
+        || playerChoice === 'Paper' && computerSelection === 'Scissors'
+        || playerChoice === 'Scissors' && computerSelection === 'Rock') {
+        ++computerScore;
         return loseMessage;
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') {
-        return loseMessage;
-    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock') {
-        return loseMessage;
-    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    } else if (playerChoice === computerSelection) {
         return drawMessage;
     } else {
-        return winMessage
+        ++playerScore;
+        return winMessage;
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function game() {
+
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt('Please choose Rock, Paper, or Scissors');
         const computerSelection = getComputerChoice();
+
         console.log(playRound(playerSelection, computerSelection));
+        console.log(`Current Score - Player: ${playerScore}, Computer: ${computerScore}.`);
     }
+
+    function scoreBoard() {
+    if (playerScore > computerScore) {
+        return `You win the game! Final score: ${playerScore} - ${computerScore}!`
+    } else if (playerScore == computerScore) {
+        return `Game ended in a draw! Final score: ${playerScore} - ${computerScore}!`
+    } else {
+        return `You lost the game! Final score: ${playerScore} - ${computerScore}!`
+    }
+}
+
+    console.log(scoreBoard());
 }
 
 game();
