addd69c971ed5d011defc1e1fd4bc0b0eccd7f7c
kavain85 <gruye2@gmail.com>
2022-08-15 19:21:06 -0700

    Add UI elements, game logic, choice logic

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 
 This is the Rock, Paper, Scissors exercise for The Odin Project. It is meant to test my ability to create a simple game from scratch using what I have learned from previous modules, and my ability to problem solve and find the information that I need to make it work.
 
-08/13/22
+__08/13/22__
+
 Began work on adding UI elements, with buttons for each selection, so the user does not have to input at a prompt, and eventually results will output to page elements instead of browser console.
+
+__08/15/22__
+
+Added:
+- Function for getting computer choice
+- Button event handlers to play a round upon clicking a button
+- Function for playing a round, and output to console (for testing)
+- Additional HTML elements for UI (player/computer choices, scoreboard)
+- CSS styling to buttons, choices, and scoreboard (in progress)
--- script.js
+++ script.js
@@ -1,40 +1,52 @@
-const btnRock = document.querySelector('#rock');
-const btnPaper = document.querySelector('#paper');
-const btnScissors = document.querySelector('#scissors');
+//  grabs buttons for use in script
+const choices = document.querySelectorAll('button');
 
-
-
-
-// Learned how to create an arry, then select a random value
-const rps = ['Rock', 'Paper', 'Scissors'];
-
-// Scores start at zero
-let playerScore = 0;
-let computerScore = 0;
-
-// Gets computers choice for each round
+//  Gets computer choice, works
 function getComputerChoice() {
-    return rps[Math.floor(Math.random()*rps.length)]
+    const rps = ['Rock', 'Paper', 'Scissors'];
+    return rps[Math.floor(Math.random() * rps.length)];
+} 
+
+function getPlayerChoice() {
+    choices.forEach((button) => {
+        button.addEventListener('click', (playerChoice) => {
+            return button.textContent;
+        });
+    });
 }
 
-// Plays a round, and grants points to winner of round
-function playRound(playerSelection, computerSelection) {
-
-    let playerChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase();
-    
-    const winMessage = `You win! ${playerChoice} beats ${computerSelection}!`;
-    const loseMessage = `You lose! ${computerSelection} beats ${playerChoice}!`;
-    const drawMessage = `Round is a draw! You chose: ${playerChoice}, Computer chose: ${computerSelection}`;
-
-    if (playerChoice === 'Rock' && computerSelection === 'Paper'
-        || playerChoice === 'Paper' && computerSelection === 'Scissors'
-        || playerChoice === 'Scissors' && computerSelection === 'Rock') {
-        ++computerScore;
-        return loseMessage;
-    } else if (playerChoice === computerSelection) {
-        return drawMessage;
-    } else {
-        ++playerScore;
-        return winMessage;
-    }
+// Plays a round when user chooses and clicks a button
+function playRound() {
+    //  does not work
+    let playerChoice = getPlayerChoice();
+    //  works
+    let computerChoice = getComputerChoice();
+    //  works
+    console.log(playerChoice+' - '+computerChoice);
 }
+
+// Fires off playRound function, works
+choices.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound();
+    });
+});
+
+
+// Original playRound code from console version
+// function playRound(playerChoice, computerSelection) {
+//  const winMessage = `You win! ${playerChoice} beats ${computerSelection}!`;
+//  const loseMessage = `You lose! ${computerSelection} beats ${playerChoice}!`;
+//  const drawMessage = `Round is a draw! You chose: ${playerChoice}, Computer chose: ${computerSelection}`;
+//  if (playerChoice === 'Rock' && computerSelection === 'Paper'
+//      || playerChoice === 'Paper' && computerSelection === 'Scissors'
+//      || playerChoice === 'Scissors' && computerSelection === 'Rock') {
+//      ++computerScore;
+//      return loseMessage;
+//  } else if (playerChoice === computerSelection) {
+//      return drawMessage;
+//  } else {
+//      ++playerScore;
+//      return winMessage;
+//  }
+// }
