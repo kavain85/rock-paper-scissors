9245b645392f42609b5d54dcf2d80fc992854873
kavain85 <gruye2@gmail.com>
2022-08-18 15:50:03 -0700

    Rename variables

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //  grabs buttons for use in script, declares variables
 const choiceButtons = document.querySelectorAll('.choice');
-const pChoiceDisplay = document.querySelector('#p-choice');
-const cChoiceDisplay = document.querySelector('#c-choice');
-const pScoreDisplay = document.querySelector('#p-score');
-const cScoreDisplay = document.querySelector('#c-score');
-const winnerName = document.querySelector('#winner-name');
-const winStatus = document.querySelector('#win-status');
+const pChoiceDisplayElement = document.querySelector('#p-choice');
+const cChoiceDisplayElement = document.querySelector('#c-choice');
+const pScoreDisplayElement = document.querySelector('#p-score');
+const cScoreDisplayElement = document.querySelector('#c-score');
+const winnerNameElement = document.querySelector('#winner-name');
+const winStatusElement = document.querySelector('#win-status');
 let buttonText;
-let playerScore;
-let computerScore;
+let pScore;
+let cScore;
 
 //  Gets computer choice
 function getComputerChoice() {
@@ -24,31 +24,46 @@ choiceButtons.forEach((button) => {
 });
 
 // Plays a round when user chooses and clicks a button
-function playRound(playerChoice, computerChoice) {
-    pChoiceOutput = pChoiceDisplay;
-    cChoiceOutput = cChoiceDisplay;
-    pScoreOutput = pScoreDisplay;
-    cScoreOutput = cScoreDisplay;
-    playerChoice = buttonText;
+function playRound() {
+    pChoiceOutput = pChoiceDisplayElement;
+    cChoiceOutput = cChoiceDisplayElement;
+    pScoreOutput = pScoreDisplayElement;
+    cScoreOutput = cScoreDisplayElement;
+    pChoiceText = buttonText;
     cChoiceOutput.textContent = getComputerChoice();
-    pChoiceOutput.textContent = playerChoice;
-        
+    pChoiceOutput.textContent = pChoiceText;
+    pChoice = pChoiceOutput.textContent;
+    cChoice = cChoiceOutput.textContent
+    // cChoiceOutput.textContent = getComputerChoice();
+    // pChoiceOutput.textContent = buttonText;
+    // pChoice = pChoiceOutput.textContent(buttonText);
+    // cChoice = cChoiceOutput.textContent(getComputerChoice);
+    winnerName = winnerNameElement.textContent;
+
+    roundDraw();
+
     // const winMessage = `You chose: ${playerChoice}. Computer chose: ${computerChoice}. You win!`;
     // const loseMessage = `You chose: ${playerChoice} \r\nComputer chose: ${computerChoice} \nYou lose!`;
     // const drawMessage = `You chose: ${playerChoice} \r\nComputer chose: ${computerChoice} \nRound is a draw!`;
-    // if (playerChoice === 'Rock' && computerChoice === 'Paper'
-    //     || playerChoice === 'Paper' && computerChoice === 'Scissors'
-    //     || playerChoice === 'Scissors' && computerChoice === 'Rock') {
-    //     // ++computerScore;
-    //     pChoiceOutput.textContent = playerChoice;
-    // } else if (playerChoice === computerChoice) {
-    //     // choiceOutput.textContent = drawMessage;
-    // } else {
-    //     // ++playerScore;
-    //     // choiceOutput.textContent = winMessage;
-    // }
+    if (pChoice === 'Rock' && cChoice === 'Paper'
+        || pChoice === 'Paper' && cChoice === 'Scissors'
+        || pChoice === 'Scissors' && cChoice === 'Rock') {
+        
+        // ++computerScore;
+    } else if (pChoice === cChoice) {
+        // choiceOutput.textContent = drawMessage;
+    } else {
+        // ++playerScore;
+        
+        // choiceOutput.textContent = winMessage;
+    }
 
-    console.log(`Player: ${playerChoice}, Computer: ${cChoiceOutput.textContent}`);
+    console.log(`Player: ${pChoice}, Computer: ${cChoice}`);
+
+    function roundDraw() {
+        winnerName = 'Round';
+        winStatus = 'Draw';
+    }
 }
 
 // Fires off playRound function, similar to game() in original version
