4877c199c4f4e7c4fff134eb3e7d7004aac80a5b
kavain85 <gruye2@gmail.com>
2022-08-19 13:34:54 -0700

    Add game ending logic, clean up global variables

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-//  grabs buttons for use in script, declares variables
-const choiceButtons = document.querySelectorAll('.choice');
-const pChoiceDisplayElement = document.querySelector('#p-choice');
-const cChoiceDisplayElement = document.querySelector('#c-choice');
-const pScoreDisplayElement = document.querySelector('#p-score');
-const cScoreDisplayElement = document.querySelector('#c-score');
-const winnerNameElement = document.querySelector('#winner-name');
-const winStatusElement = document.querySelector('#win-status');
-const gameResultsElement = document.querySelector('#game-results');
-const resetButton = document.querySelector('#reset')
-let buttonText;
-let pScore = 0;
-let cScore = 0;
-let numRounds;
+var { choiceButtons, buttonText, numRounds, pChoiceDisplayElement, cChoiceDisplayElement, pScoreDisplayElement, cScoreDisplayElement, winnerNameElement, winStatusElement, cScore, pScore, gameResultsElement, resetButton } = rpsVariables();
+
+function rpsVariables() {
+    const choiceButtons = document.querySelectorAll('.choice');
+    const pChoiceDisplayElement = document.querySelector('#p-choice');
+    const cChoiceDisplayElement = document.querySelector('#c-choice');
+    const pScoreDisplayElement = document.querySelector('#p-score');
+    const cScoreDisplayElement = document.querySelector('#c-score');
+    const winnerNameElement = document.querySelector('#winner-name');
+    const winStatusElement = document.querySelector('#win-status');
+    const gameResultsElement = document.querySelector('#game-results');
+    const resetButton = document.querySelector('#reset');
+    let buttonText;
+    let pScore = 0;
+    let cScore = 0;
+    let numRounds = 0;
+    return { choiceButtons, buttonText, numRounds, pChoiceDisplayElement, cChoiceDisplayElement, pScoreDisplayElement, cScoreDisplayElement, winnerNameElement, winStatusElement, cScore, pScore, gameResultsElement, resetButton };
+}
 
 //  Gets computer choice
 function getComputerChoice() {
@@ -19,12 +23,16 @@ function getComputerChoice() {
     return rps[Math.floor(Math.random() * rps.length)];
 } 
 
-
-//  Grabs button text and sends value to buttonText variable when player clicks a button
 choiceButtons.forEach((button) => {
     button.addEventListener('click', () => {
         buttonText = button.textContent;
-        playRound();
+        if (numRounds < 4) {
+            playRound();
+            numRounds++;
+        } else {
+            playRound();
+            gameOver();
+        }
     })});
 
 // Plays a round when user chooses and clicks a button
@@ -35,12 +43,13 @@ function playRound() {
     cScoreOutput = cScoreDisplayElement;
     pChoiceText = buttonText;
     cChoiceOutput.textContent = getComputerChoice();
-    pChoiceOutput.textContent = pChoiceText;
+    pChoiceOutput.textContent = buttonText;
     pChoice = pChoiceOutput.textContent;
     cChoice = cChoiceOutput.textContent;
     winnerName = winnerNameElement;
     winStatus = winStatusElement;
 
+    // Determines round outcome and displays winner of round
     if (pChoice === 'Rock' && cChoice === 'Paper'
         || pChoice === 'Paper' && cChoice === 'Scissors'
         || pChoice === 'Scissors' && cChoice === 'Rock') {
@@ -59,7 +68,8 @@ function playRound() {
     }
 };
 
-function gameWinner() {
+// Determines game winner and displays message
+function gameOver() {
     if (pScore > cScore) {
         gameResultsElement.textContent = `Player wins game with score of ${pScore} to ${cScore}!`;
         gameResultsElement.style.color = `green`;
@@ -70,9 +80,10 @@ function gameWinner() {
         gameResultsElement.textContent = `Game ended in a draw! Final score was ${pScore} to ${cScore}!`;
         gameResultsElement.style.color = `cyan`;
     }
-    resetButton.style.visibility = 'visible';
+    resetButton.style.visibility = 'visible'; // Makes reset button visible
 };
 
+// Reset button to reload page and restart game
 resetButton.addEventListener('click', () => {
     window.location.reload();
 });
